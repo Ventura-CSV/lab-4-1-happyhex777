4eb6c94fe72f74ebf922c66a7d963e8b8e116caf
Hector Diaz <hhectordiazz@icloud.com>
2024-06-30 01:35:05 -0700

    4-1 complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-
+    # Input the number
     N = int(input('Enter the number N: '))
     result = []
 
@@ -8,6 +8,15 @@ def main():
     Code Your Program here
     ########################################
     """
+    # create a definiton for "calculate_power_of_2" in order to calculate result
+    def calculate_powers_of_2(n):
+        result=[] # initialize an empty list named "result" to store the powers of 2
+        for i in range(n + 1): # use the range(n+1) function to create a sequence of numbers from 0 to N
+            result.append(2 ** i)# for each value of i in the loop, calculate 2 raised to the power of i
+        return result #return the list of powers of 2
+    result = calculate_powers_of_2(N)
+    print(result)
+    
 
     ########################################
     # Do not delete the return statement
